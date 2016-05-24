bbff500c2f9654e532dbdc7dc7faabc854ca0ff7
changwinstonr <chang.winstonr@gmail.com>
2016-05-24 00:27:03 -0700

    Adds: OnClick.

--- app/src/main/java/winston/weekendlab_api/MainActivity.java
+++ app/src/main/java/winston/weekendlab_api/MainActivity.java
@@ -1,16 +1,61 @@
 package winston.weekendlab_api;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+//TODO: Stretch (Extended): 1) Test cases 2) Material design 3) Accessiblity 4) OAuth
 
-    //TODO: 
+public class MainActivity extends AppCompatActivity implements ScryApi.ApiResponseHandler {
+
+    //TODO: Initialize XML View variables
+    TextView textView;
+    EditText scryEntry;
+    ImageView scryImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: scry button, onClickListener, & inputMethod mgr
+        final Button onScry = (Button) findViewById(R.id.scryButton);
+        assert onScry != null; //in case an AssertionError is thrown
+
+        onScry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scryEntry = (EditText) findViewById(R.id.scryEntry);
+
+                InputMethodManager iMM = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                //hide the keyboard on search
+                iMM.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),iMM.HIDE_NOT_ALWAYS);
+
+                int scryIndex = Integer.parseInt(scryEntry.getText().toString());
+                if(scryIndex <= 0 || 410064 < scryIndex){ //last card of the most recent set Shadows over Innistrad
+                    onScry.setClickable(false);
+                    Toast.makeText(MainActivity.this, "Scry has been bounced. Try scrying again.", Toast.LENGTH_LONG).show(); //Planeswalker fail.
+                } else {
+                    onScry.setClickable(true);
+                    ScryApi.getInstance(MainActivity.this).doRequest(scryIndex);
+                }
+            }
+        });//end OnClickListener
+    }
+
+    @Override
+    public void handleResponse(String response) {
+        //loads scryImage into the ImageView
+        scryImage = (ImageView) findViewById(R.id.scryImage);
+        Picasso.with(MainActivity.this).load(response).into(scryImage);
     }
 }
--- app/src/main/java/winston/weekendlab_api/ScryApi.java
+++ app/src/main/java/winston/weekendlab_api/ScryApi.java
@@ -1,7 +1,7 @@
 package winston.weekendlab_api;
 
 //TODO For Flickr: Implement key: a349634211c1df1171b6a4070099f108 | Secret: 2c9abbdf5789a87b
-//TODO For MTG: http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=[1-368970]&type=card
+//TODO For MTG: http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=[1-410064]&type=card
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -11,27 +11,27 @@ import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
-public class api_flickr {
-    private static api_flickr instance;
+public class ScryApi {
+    private static ScryApi instance;
     private static ApiResponseHandler responseHandler;
 
-    //private api_flickr(){}
+    //private ScryApi(){}
 
     //implements Response Hndler
-    public static api_flickr getInstance(ApiResponseHandler handler){
+    public static ScryApi getInstance(ApiResponseHandler handler){
         responseHandler = handler;
         if (instance == null) {
-            instance = new api_flickr();
+            instance = new ScryApi();
         } return instance;}//end handler
 
-    public void jsonRequest(int urlIndex){
+    public void doRequest(int urlIndex){
         AsyncHttpClient client = new AsyncHttpClient();
         //TODO: retrieve multiverseid &&|| image
-        client.get("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + jsonRequest + "&type=card", null, new JsonHttpResponseHandler(){
-            //TODO: Try/Catch
-            public void onSuccess(int errorCode, Header[] headers, JSONObject response){
+        client.get("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + urlIndex + "&type=card", null, new JsonHttpResponseHandler(){
+        //TODO: Try/Catch
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response){
                 try {
-                    responseHandler.handleResponse(response.getString("multiverseid"));
+                    responseHandler.handleResponse(response.getString("Image"));
                 } catch (JSONException e){
                     e.printStackTrace();
                 }
